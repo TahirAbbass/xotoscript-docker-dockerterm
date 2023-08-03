b6b7d72ae16af21779c8d64808f3bf66e325819a
gregoire cousin <gregoire.cousin@loreal.com>
2023-08-03 11:40:34 +0200

    blast off

--- test/docker/build_test.py
+++ test/docker/build_test.py
@@ -3,8 +3,7 @@ import platform
 import subprocess
 import pytest
 
-#ENV
-
+# env
 HOME_PATH = os.environ['HOME']
 ROOT = 'root'
 USER_NAME = 'xotosphere'
@@ -12,10 +11,9 @@ USER_GROUP = 'dockerterm'
 FONTS_PATH = '/usr/local/share/fonts'
 OH_MY_ZSH_PATH = HOME_PATH + '/.oh-my-zsh'
 
-
 import pytest
 
-# Sample data from the .env file
+# sample data from the .env file
 env_data = {
     'NEOVIM_VERSION': '0.9.0',
     'YARN_VERSION': '1',
@@ -26,29 +24,29 @@ env_data = {
     'GITSTATUS_VERSION': '1.0.0',
 }
 
+# test if the package is installed and the version is correct
 @pytest.mark.parametrize('package,expected_version', [
     ('nvim', env_data['NEOVIM_VERSION']),
     ('fzf', env_data['FZF_VERSION']),
 ])
 
+# test if the package is installed and the version is correct
 def test_nvm_version(host, package, expected_version):
-
     try:
-        # Source the environment in a subshell and run the --version command
-        cmd = f'{package} --version'
+        cmd = f'{package} --version' # source the environment in a subshell and run the --version command
         cmd_output = subprocess.run(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
-
-        # Check if the command was successful and capture the output
-        if cmd_output.returncode == 0:
-            nvm_version = cmd_output.stdout.strip()  # Remove leading/trailing spaces and newlines
+        if cmd_output.returncode == 0: # check if the command was successful and capture the output
+            nvm_version = cmd_output.stdout.strip()  # remove leading/trailing spaces and newlines
             assert expected_version in nvm_version
         else:
-            pytest.fail(f"Error executing {cmd}. Is installed?")
+            pytest.fail(f"error executing {cmd}. is installed?")
     except FileNotFoundError:
-        pytest.fail("executable not found. Is properly sourced?")
+        pytest.fail("executable not found. is properly sourced?")
 
+# test if the package is installed and the version is correct
 def test_java_version(host):
     expected_version = 'openjdk 11'
     cmd_output = subprocess.check_output(['java', '--version'], stderr=subprocess.STDOUT, text=True)
     java_version = cmd_output.splitlines()[0].strip()
     assert java_version.startswith(expected_version)
+
